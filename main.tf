108e6355ae307bf1f8f709bc7f567c9c0609d557
Ferronn <ferronn@ferronn.dev>
2022-11-28 16:32:27 +0000

    import project iam policy

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
-provider "google" {
+locals {
   project = "www-ferronn-dev"
   region  = "us-central1"
   zone    = "us-central1-c"
+}
+
+provider "google" {
+  project = local.project
+  region  = local.region
+  zone    = local.zone
 }
 
 resource "google_storage_bucket" "static" {
@@ -28,7 +34,7 @@ data "google_container_registry_image" "nginx" {
 
 resource "google_cloud_run_service" "nginx" {
   name     = "nginx"
-  location = "us-central1"
+  location = local.region
   lifecycle {
     ignore_changes = [
       template[0].metadata[0].annotations["run.googleapis.com/client-name"],
@@ -67,11 +73,67 @@ resource "google_cloud_run_service" "nginx" {
 
 resource "google_cloud_run_domain_mapping" "nginx" {
   name     = "ferronn.dev"
-  location = "us-central1"
+  location = local.region
   metadata {
-    namespace = "www-ferronn-dev"
+    namespace = local.project
   }
   spec {
     route_name = google_cloud_run_service.nginx.name
   }
+}
+
+data "google_iam_policy" "project" {
+  binding {
+    members = [
+      "serviceAccount:715563492971@cloudbuild.gserviceaccount.com",
+    ]
+    role = "roles/cloudbuild.builds.builder"
+  }
+  binding {
+    members = [
+      "serviceAccount:service-715563492971@gcp-sa-cloudbuild.iam.gserviceaccount.com",
+    ]
+    role = "roles/cloudbuild.serviceAgent"
+  }
+  binding {
+    members = [
+      "serviceAccount:service-715563492971@gcf-admin-robot.iam.gserviceaccount.com",
+    ]
+    role = "roles/cloudfunctions.serviceAgent"
+  }
+  binding {
+    members = [
+      "serviceAccount:service-715563492971@gcp-sa-cloudscheduler.iam.gserviceaccount.com",
+    ]
+    role = "roles/cloudscheduler.serviceAgent"
+  }
+  binding {
+    members = [
+      "serviceAccount:service-715563492971@compute-system.iam.gserviceaccount.com",
+    ]
+    role = "roles/compute.serviceAgent"
+  }
+  binding {
+    members = [
+      "serviceAccount:service-715563492971@containerregistry.iam.gserviceaccount.com",
+    ]
+    role = "roles/containerregistry.ServiceAgent"
+  }
+  binding {
+    members = [
+      "serviceAccount:715563492971@cloudservices.gserviceaccount.com",
+    ]
+    role = "roles/editor"
+  }
+  binding {
+    members = [
+      "serviceAccount:service-715563492971@serverless-robot-prod.iam.gserviceaccount.com",
+    ]
+    role = "roles/run.serviceAgent"
+  }
+}
+
+resource "google_project_iam_policy" "project" {
+  project     = local.project
+  policy_data = data.google_iam_policy.project.policy_data
 }
